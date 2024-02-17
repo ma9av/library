634698e653bf79a97b4e02c0d72d7d16f6bdef61
ma9av <manavpanchal42@gmail.com>
2024-02-18 01:21:32 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+if (process.env.NODE_ENV !== 'production')
+{
+    require('dotenv').config()
+}
+
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+const expressLayouts = require('express-ejs-layouts')
+
+
+
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views',__dirname + '/views')
+app.set('layout','layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+
+db.on('error', err => console.error(err))
+db.once('open', ()=>  console.log('mongodb connected'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
