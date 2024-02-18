9f0df523d321f9624ce17a17680a64c4e5e5f30a
ma9av <manavpanchal42@gmail.com>
2024-02-19 01:21:36 +0530

    commit

--- server.js
+++ server.js
@@ -12,12 +12,17 @@ const expressLayouts = require('express-ejs-layouts')
 
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author')
 
 app.set('view engine', 'ejs')
 app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+
+app.use(express.urlencoded({ extended: true }));
+ app.use(express.json());
 
 
 mongoose.connect(process.env.DATABASE_URL)
@@ -27,5 +32,8 @@ db.on('error', err => console.error(err))
 db.once('open', ()=>  console.log('mongodb connected'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
+
+
